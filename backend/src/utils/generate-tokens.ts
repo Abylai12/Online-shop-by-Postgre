fdf2cb107e6e120373a46a004197eb28ed2397fe
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2025-02-08 14:18:35 +0800

    cookies setup

--- backend/src/utils/generate-tokens.ts
+++ backend/src/utils/generate-tokens.ts
@@ -67,14 +67,14 @@ export const setCookies = (
 ) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax", // prevents CSRF attack, cross-site request forgery attack
+    secure: true,
+    sameSite: "none", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax", // prevents CSRF attack, cross-site request forgery attack
+    secure: true,
+    sameSite: "none", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
